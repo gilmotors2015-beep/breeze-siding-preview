19d8fccf257ac2cbbb03d3240098495f387e5218
gilmotors2015-beep <gilmotors2015@gmail.com>
2026-05-10 21:54:10 -0700

    Add Supabase private admin config

---
+++ admin/private-admin-config.js
@@ -0,0 +1,7 @@
+window.BREEZE_PRIVATE_ADMIN = {
+  enabled: true,
+  provider: 'supabase',
+  supabaseUrl: 'https://nwvsriwsbpdhszmmousi.supabase.co',
+  supabaseAnonKey: 'sb_publishable_SHsFk0DcYRACTjzr_xZsAA_e-wX-Vt7',
+  ownerEmail: 'gilmotors2015@gmail.com'
+};
